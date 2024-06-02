7daa00708083cc53cb91fd193d3c21c2f8cf4f2b
Samuel Alegre <41902278+samuelalecas@users.noreply.github.com>
2024-06-02 14:57:12 +0200

    Fix build error

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -5,7 +5,7 @@ export const Projects = () => {
   return (
     <section className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8 max-w-[1500px]">
       {projectData.map((project) => (
-        <ProjectCard data={project} />
+        <ProjectCard key={project.title} data={project} />
       ))}
     </section>
   );
